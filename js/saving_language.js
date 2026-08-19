1bfa7d89e90150709000834a7e1a5d146881e2c5
spiderasasen <spiderasasen@gmail.com>
2026-08-19 12:55:03 -0400

    got the main simple system down

--- js/saving_language.js
+++ js/saving_language.js
@@ -1,1 +1,7 @@
-console.log("saving_language.js");
+const saved_language = document.getElementById("language-select");
+const contuine = document.getElementById("continue-button");
+
+contuine.onclick = () => {
+    console.log(saved_language.value);
+    localStorage.setItem("language", saved_language.value);
+};
