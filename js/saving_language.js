b9438f26e3d923d83ee13e5b6906d9828ed35d10
spiderasasen <spiderasasen@gmail.com>
2026-08-19 13:10:52 -0400

    got the main section down for good. i forgot to add the level of the user

--- js/saving_language.js
+++ js/saving_language.js
@@ -1,7 +1,24 @@
 const saved_language = document.getElementById("language-select");
+const saved_language_level = document.getElementById("language-level");
 const contuine = document.getElementById("continue-button");
+const levelSection = document.getElementById("language-level-selection");
 
+// hide on load
+levelSection.style.display = "none";
+
+// show/hide when user selects a language
+saved_language.addEventListener("change", () => {
+    if (saved_language.value !== "") {
+        levelSection.style.display = "block";
+    } else {
+        levelSection.style.display = "none";
+    }
+});
+
+//simple contuine section
 contuine.onclick = () => {
     console.log(saved_language.value);
+    console.log(saved_language_level.value);
     localStorage.setItem("language", saved_language.value);
+    localStorage.setItem("language-level", saved_language_level.value);
 };
